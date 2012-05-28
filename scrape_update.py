e182ad24883c64efceeb68f2d8719afe964fc3dd
droquo <drew.s.gordon@gmail.com>
2012-05-28 00:16:05 -0500

    python scraper

---
+++ scrape_update.py
@@ -0,0 +1,17 @@
+from pymongo import *
+import BeautifulSoup
+
+#connect to db and wikiCollection
+
+
+#loop through documents by time
+
+##get url from each documents
+
+##scrape that url and get the diff text
+
+##save diff text
+
+##update to include text with each documents
+
+##repeat
