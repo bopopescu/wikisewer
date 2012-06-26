c4fca8510d3376b7b26987c7d7f84905655802d8
droquo <drew.s.gordon@gmail.com>
2012-06-26 09:28:12 -0500

    time to get this mess cleaned up

--- scrape_update.py
+++ scrape_update.py
@@ -39,47 +39,50 @@ try:
 			#try:
 			req = urllib2.Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5'})
 			data = urllib2.urlopen(req).read()
-			soup = BeautifulSoup.BeautifulSoup(data, convertEntities=BeautifulSoup.BeautifulSoup.HTML_ENTITIES)
+			#soup = BeautifulSoup.BeautifulSoup(data, convertEntities=BeautifulSoup.BeautifulSoup.HTML_ENTITIES)
+			soup = BeautifulSoup.BeautifulSoup(data)
 			#for now only give us the deletions of vandalism on not the vandalistic delections
-			if message['delta'] < 0:
+			#if message['delta'] < 0:
 				##scrape that url and get the diff text
 				#txt = soup.find('td', {'class':'diff-deletedline'}).getText('\n')
-				try:
-					#TODO 25JUN2012 - fix this scraping business here
-					#txt = [td.find('span', {'class':'diffchange diffchange-inline'}).getText('\n') for td in soup.findAll('td', {'class':'diff-deletedline'})]
-					txt = [td.findAll('div') for td in soup.findAll('td', {'class':'diff-deletedline'})]
-					txt2 = [td.findAll('div') for td in soup.findAll('td', {'class':'diff-addedline'})]		
-					nest = [td.find('img') for td in soup.findAll('a', { 'class': 'image'} )]
-					imgs = [x['src'] for x in nest]
-					if len(imgs)>0:
-						img = "http:" + imgs[0]
-					else:
-						img = "no image"
-
-					#vandal_text = ', '.join(txt)
-					txt = txt.string
-					txt2 = txt2.string
-					print txt
-					print txt2
-					print '===',message['page'],'==='
-					#print vandal_text
-					print img
-					#time.sleep(10)
-				except AttributeError as e:
-					print 'something wrong with the text', e
-					txt = "This vandalism is not available right now."
-					print txt
-					continue
-			else:
-				print '===',message['page'],'==='
-				print 'Not a deletion'
-				txt = 'Not a deletion'
+			try:
+				#TODO 25JUN2012 - fix this scraping business here
+				#txt = [td.find('span', {'class':'diffchange diffchange-inline'}).getText('\n') for td in soup.findAll('td', {'class':'diff-deletedline'})]
+				txt = [td.findAll('div') for td in soup.findAll('td', {'class':'diff-deletedline'})]
+				txt2 = [td.findAll('div') for td in soup.findAll('td', {'class':'diff-addedline'})]		
 				nest = [td.find('img') for td in soup.findAll('a', { 'class': 'image'} )]
 				imgs = [x['src'] for x in nest]
 				if len(imgs)>0:
 					img = "http:" + imgs[0]
 				else:
 					img = "no image"
+				
+				txt = str(txt).strip('[[]]')				
+				txt2 = str(txt2).strip('[[]]')
+				#txt = txt.string
+				#txt2 = txt2.string
+
+				print txt
+				print txt2
+				print '===',message['page'],'==='
+				#print vandal_text
+				print img
+				#time.sleep(10)
+			except AttributeError as e:
+				print 'something wrong with the text', e
+				txt = "This vandalism is not available right now."
+				print txt
+				continue
+			#else:
+				#print '===',message['page'],'==='
+				#print 'Not a deletion'
+				#txt = 'Not a deletion'
+				#nest = [td.find('img') for td in soup.findAll('a', { 'class': 'image'} )]
+				#imgs = [x['src'] for x in nest]
+				#if len(imgs)>0:
+				#	img = "http:" + imgs[0]
+				#else:
+				#	img = "no image"
 	
 			collection_out.insert({'time':message['time'], 
 								   'diff_url':message['url'], 
